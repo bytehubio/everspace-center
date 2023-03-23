ee1918bf95337bb9e6f7c44a20ea109f6e1a7058
nerzh <emptystamp@gmail.com>
2023-03-23 09:29:13 +0100

    fix deploy

--- deploy.rb
+++ deploy.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/env ruby
 
-name = "everspace.center"
-system("eval \"$(ssh-agent -s)\" || true && ssh-add ~/.ssh/#{name}.pub || true && git reset --hard HEAD || true && git pull origin master")
+ssh_key_name = "everspace.center"
+system("eval \"$(ssh-agent -s)\" || true && ssh-add ~/.ssh/#{ssh_key_name}.pub || true && ssh-add ~/.ssh/#{ssh_key_name} || true && git reset --hard HEAD || true && git pull origin master")
 system("swift package resolve")
 #system("swift package update")
 system("swift build -c release -Xswiftc -Ounchecked -Xswiftc -whole-module-optimization -Xcc -O2")
 #system("swift build -c debug")
-system("pkill -9 -f #{name}")
+system("pkill -9 -f #{ssh_key_name}")
 # system("/home/devton/swift/#{name}/.build/release/#{name} --env production > /home/devton/swift/#{name}/log.txt 2>&1 &")
-system("sudo supervisorctl restart #{name}-app")
+system("sudo supervisorctl restart #{ssh_key_name}-app")
